dd74417cc1b5ce72f0bb76ad637915b1539cc759
Ganesh <pwork11421@gmail.com>
2026-02-16 16:14:02 +0530

    Delete ERP directory

--- ERP/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.3.2" apply false
-    kotlin("android") version "2.0.21" apply false
-    kotlin("plugin.serialization") version "2.0.21" apply false
-    id("io.objectbox") version "4.0.3" apply false 
-}
